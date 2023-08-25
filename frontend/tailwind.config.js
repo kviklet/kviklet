c295bc249441b80c151c7f277802e8b695ae7387
Jascha <bestejascha@gmail.com>
2023-08-25 19:23:24 +0200

    Change font to linear, fix creating requests

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -3,6 +3,11 @@ module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {},
+    fontFamily: {
+      sans: ["Inter", "sans-serif"],
+      serif: ["Manrope", "serif"],
+      mono: ["Roboto Mono", "monospace"],
+    },
   },
   plugins: [require("tailwind-scrollbar")({ nocompatible: true })],
 };
