dd2ef292d6ca153238f840d96124b35d32c712bf
Jascha <bestejascha@gmail.com>
2023-10-17 16:01:49 +0200

    Run init.sql in e2e tests

---
+++ e2e/init.sql
@@ -0,0 +1,48 @@
+create table public.test
+(
+    id   integer,
+    test varchar
+);
+
+alter table public.test
+    owner to postgres;
+
+INSERT INTO public.test (id, test) VALUES (2, 'blu');
+INSERT INTO public.test (id, test) VALUES (54, 'bli');
+INSERT INTO public.test (id, test) VALUES (32, 'cookies');
+INSERT INTO public.test (id, test) VALUES (11, 'sample_text1');
+INSERT INTO public.test (id, test) VALUES (22, 'sample_text2');
+INSERT INTO public.test (id, test) VALUES (111, 'sample_text1');
+INSERT INTO public.test (id, test) VALUES (222, 'sample_text2');
+INSERT INTO public.test (id, test) VALUES (333, 'sample_text3');
+INSERT INTO public.test (id, test) VALUES (1, 'some new value');
+INSERT INTO public.test (id, test) VALUES (111, 'sample_text1');
+INSERT INTO public.test (id, test) VALUES (222, 'sample_text2');
+INSERT INTO public.test (id, test) VALUES (333, 'sample_text3');
+INSERT INTO public.test (id, test) VALUES (2, 'blu');
+INSERT INTO public.test (id, test) VALUES (54, 'bli');
+INSERT INTO public.test (id, test) VALUES (32, 'cookies');
+INSERT INTO public.test (id, test) VALUES (11, 'sample_text1');
+INSERT INTO public.test (id, test) VALUES (22, 'sample_text2');
+INSERT INTO public.test (id, test) VALUES (111, 'sample_text1');
+INSERT INTO public.test (id, test) VALUES (222, 'sample_text2');
+INSERT INTO public.test (id, test) VALUES (333, 'sample_text3');
+INSERT INTO public.test (id, test) VALUES (1, 'some new value');
+INSERT INTO public.test (id, test) VALUES (111, 'sample_text1');
+INSERT INTO public.test (id, test) VALUES (222, 'sample_text2');
+INSERT INTO public.test (id, test) VALUES (333, 'sample_text3');
+INSERT INTO public.test (id, test) VALUES (2, 'blu');
+INSERT INTO public.test (id, test) VALUES (54, 'bli');
+INSERT INTO public.test (id, test) VALUES (32, 'cookies');
+INSERT INTO public.test (id, test) VALUES (11, 'sample_text1');
+INSERT INTO public.test (id, test) VALUES (22, 'sample_text2');
+INSERT INTO public.test (id, test) VALUES (111, 'sample_text1');
+INSERT INTO public.test (id, test) VALUES (222, 'sample_text2');
+INSERT INTO public.test (id, test) VALUES (333, 'sample_text3');
+INSERT INTO public.test (id, test) VALUES (1, 'some new value');
+INSERT INTO public.test (id, test) VALUES (111, 'sample_text1');
+INSERT INTO public.test (id, test) VALUES (222, 'sample_text2');
+INSERT INTO public.test (id, test) VALUES (333, 'sample_text3');
+INSERT INTO public.test (id, test) VALUES (33, 'a new value');
+INSERT INTO public.test (id, test) VALUES (33, 'a new value');
+INSERT INTO public.test (id, test) VALUES (33, 'a new value');
