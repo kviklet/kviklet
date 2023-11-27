0af0ca01cddcc8b5c36e5f7a9c20be3b0565b53c
Jascha <bestejascha@gmail.com>
2023-11-27 07:41:31 +0100

    Add forwarded header filter spring

---
+++ backend/src/main/kotlin/dev/kviklet/kviklet/security/RequestLoggingFilterConfig.kt
@@ -0,0 +1,20 @@
+package dev.kviklet.kviklet.security
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.filter.CommonsRequestLoggingFilter
+
+@Configuration
+class RequestLoggingFilterConfig {
+
+    @Bean
+    fun logFilter(): CommonsRequestLoggingFilter {
+        val filter = CommonsRequestLoggingFilter()
+        filter.setIncludeQueryString(true)
+        filter.setIncludePayload(true)
+        filter.setMaxPayloadLength(10000)
+        filter.setIncludeHeaders(true)
+        filter.setAfterMessagePrefix("REQUEST DATA : ")
+        return filter
+    }
+}
