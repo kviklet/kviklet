f38da728b9348041ba869185eb53b554e29b38bc
Dan Nguyen <danni.le@me.com>
2023-12-14 11:23:01 +0100

    Improved code style to pass prettier check

--- frontend/src/layout/TopBanner.tsx
+++ frontend/src/layout/TopBanner.tsx
@@ -11,7 +11,6 @@ const navigation = [
   { name: "Settings", href: "/settings" },
 ];
 
-
 function TopBanner() {
   const themeContext = useContext<ThemeContext>(ThemeStatusContext);
 
@@ -30,23 +29,23 @@ function TopBanner() {
   return (
     <div>
       <header className="sticky w-full z-50 backdrop-blur-sm border-b border-slate-900/10 dark:border-b-slate-700 inset-x- top-0">
-        
         {/* Navigation Bar */}
-        <nav className="flex items-center justify-center p-0.5 px-2 sm:px-6" aria-label="Global">
-          
+        <nav
+          className="flex items-center justify-center p-0.5 px-2 sm:px-6"
+          aria-label="Global"
+        >
           <div className="flex items-center justify-between w-full">
-            
             {/* Logo and name */}
             <div className="flex lg:flex-1">
               <a href="#" className="-m-1.5 p-1.5 flex items-center">
                 <img className="p-1 h-11 w-auto" src={logo} alt="" />
-                <span className="ml-1 text-slate-700 dark:text-slate-50 font-semibold text-lg">Kviklet</span>
+                <span className="ml-1 text-slate-700 dark:text-slate-50 font-semibold text-lg">
+                  Kviklet
+                </span>
               </a>
             </div>
-           
-            
+
             <div className="flex justify-end items-center gap-x-2 sm:gap-x-4">
-              
               {/* Navigation Links */}
               <div className="flex items-center justify-between pr-3 sm:pr-4 border-r dark:border-slate-600 border-slate-200">
                 <div className="flex gap-x-4 sm:gap-x-9 lg:gap-x-9">
@@ -55,7 +54,7 @@ function TopBanner() {
                       key={item.name}
                       href={item.href}
                       className="font-semibold leading-6 text-slate-700 dark:text-slate-50 hover:text-sky-500 dark:hover:text-sky-400"
-                      >
+                    >
                       {item.name}
                     </a>
                   ))}
@@ -83,7 +82,7 @@ function TopBanner() {
                     ></path>
                   </svg>
                 </span>
-                
+
                 <span className="hidden dark:inline">
                   <svg viewBox="0 0 24 24" fill="none" className="w-6 h-6">
                     <path
@@ -105,24 +104,30 @@ function TopBanner() {
                   </svg>
                 </span>
               </button>
-              
+
               {/* Github and about */}
-              <a href="https://github.com/kviklet/kviklet/" target="_blank" rel="noopener noreferrer">
+              <a
+                href="https://github.com/kviklet/kviklet/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
                 <svg
                   viewBox="0 0 16 16"
                   className="h-5 w-5 text-white hover:text-red-500 dark:hover:text-sky-400 invert dark:invert-0"
                   fill="currentColor"
                   aria-hidden="true"
-                  >
+                >
                   <path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.013 8.013 0 0016 8c0-4.42-3.58-8-8-8z"></path>
                 </svg>
               </a>
 
-              <a href="https://Kviklet.dev" 
-                target="_blank" 
+              <a
+                href="https://Kviklet.dev"
+                target="_blank"
                 rel="noopener noreferrer"
-                className="hover:text-sky-500 dark:hover:text-sky-400">
-                  about
+                className="hover:text-sky-500 dark:hover:text-sky-400"
+              >
+                about
               </a>
             </div>
           </div>
