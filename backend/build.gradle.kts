4bd7bb04d336ff12a2dd34ce821a55a851f1825f
Jamie Li <lijiachi03@gmail.com>
2024-07-15 16:12:31 -0400

    upgrade some plugins version

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -4,10 +4,10 @@ import org.springframework.boot.gradle.tasks.run.BootRun
 plugins {
     id("org.springframework.boot") version "3.1.2"
     id("io.spring.dependency-management") version "1.0.15.RELEASE"
-    kotlin("jvm") version "1.9.10"
-    kotlin("plugin.spring") version "1.9.10"
-    kotlin("plugin.jpa") version "1.9.10"
-    kotlin("kapt") version "1.9.10" // needed for query-dsl
+    kotlin("jvm") version "1.9.24"
+    kotlin("plugin.spring") version "1.9.24"
+    kotlin("plugin.jpa") version "1.9.24"
+    kotlin("kapt") version "1.9.24" // needed for query-dsl
 }
 
 kapt {
