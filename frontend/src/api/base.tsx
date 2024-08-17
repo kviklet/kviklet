c4f43f306dd9ae130611781e0eedfc3d0845d266
Jascha Beste <bestejascha@gmail.com>
2024-08-17 16:55:28 +0200

    Allow non-default ports for docker container (#197)

--- frontend/src/api/base.tsx
+++ frontend/src/api/base.tsx
@@ -1,8 +1,15 @@
 import { z } from "zod";
 
 // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-member-access
-const apiBasePath = import.meta.env.VITE_API_BASE_PATH || ":8080";
-const baseUrl = `${window.location.protocol}//${window.location.hostname}${apiBasePath}`;
+const apiBasePath = import.meta.env.VITE_API_BASE_PATH;
+
+let baseUrl = "";
+if (!apiBasePath) {
+  // In dev mode locally, usually spring boot runs on 8080
+  baseUrl = `${window.location.protocol}//${window.location.hostname}:8080`;
+} else {
+  baseUrl = `${window.location.protocol}//${window.location.host}${apiBasePath}`;
+}
 
 export default baseUrl;
 
