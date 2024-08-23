b92612101943534fb8b87958c4533764b7ed5355
Jascha Beste <bestejascha@gmail.com>
2024-08-23 20:13:24 +0200

    Remove requirement of root privilliges from Docker image (#208)

--- run.sh
+++ run.sh
@@ -1,2 +1,11 @@
-nginx &
-java -jar app.jar
+#!/bin/bash
+# Start Nginx and Spring Boot Service
+nginx -g 'daemon off;' &
+
+java -jar /app/app.jar
+
+# Wait for any process to exit
+wait -n
+
+# Exit with status of process that exited first
+exit $?
