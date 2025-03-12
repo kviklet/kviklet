60538b529d730c3eb1d273ae0c78700abe1f0afa
Derk Schooltink <derk.schooltink@gmail.com>
2025-03-12 16:04:09 +0100

    Bump AWS SDK versions

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -56,8 +56,8 @@ dependencies {
     implementation("org.springframework:spring-context-support")
     implementation("com.github.jsqlparser:jsqlparser:4.9")
     implementation("io.kubernetes:client-java:20.0.1")
-    implementation("software.amazon.awssdk:rds:2.30.6")
-    implementation("software.amazon.awssdk:sts:2.30.6")
+    implementation("software.amazon.awssdk:rds:2.30.37")
+    implementation("software.amazon.awssdk:sts:2.30.37")
 
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
