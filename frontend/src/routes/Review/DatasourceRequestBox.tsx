5477ac1d04f18f615ffb3846344da7c46117b13b
Satria Dwi Putra <satria@sicepat.com>
2025-09-08 13:22:52 +0700

    fix for jobs checker github

--- frontend/src/routes/Review/DatasourceRequestBox.tsx
+++ frontend/src/routes/Review/DatasourceRequestBox.tsx
@@ -78,7 +78,7 @@ function DatasourceRequestBox({
         <span>Download as CSV</span>
       ),
     },
-    ...(request?.connection?.type === "MONGODB"
+    ...(request?.connection?.type === ConnectionType.MONGODB &&
       ? [
           {
             onClick: () => {},
