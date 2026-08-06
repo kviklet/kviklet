131806a7a8d0e80f7736421de777c25f9c4e6b9e
Jascha <bestejascha@gmail.com>
2026-08-06 23:19:35 +0200

    Remove unused MouseEvent import

--- frontend/src/routes/LiveSessionWebsockets.tsx
+++ frontend/src/routes/LiveSessionWebsockets.tsx
@@ -1,10 +1,4 @@
-import React, {
-  useContext,
-  useEffect,
-  useRef,
-  useState,
-  MouseEvent,
-} from "react";
+import React, { useContext, useEffect, useRef, useState } from "react";
 import * as monaco from "monaco-editor/esm/vs/editor/editor.api";
 import Button from "../components/Button";
 import MultiResult from "../components/MultiResult";
